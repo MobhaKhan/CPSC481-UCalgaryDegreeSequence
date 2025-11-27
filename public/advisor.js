1312cd2dcd3c54df184505ac0dd2a8b3499301de
30203751 <nebila.wako@ucalgary.ca>
2025-11-26 18:31:01 -0700

    advisro page badic htl\ml css

---
+++ public/advisor.js
@@ -0,0 +1,33 @@
+<section class="advisor-card">
+  <h2 class="advisor-title">Student Information</h2>
+
+  <label class="advisor-label">
+    <span>UCID</span>
+    <input 
+      type="text" 
+      id="advisor-ucid" 
+      placeholder="12345678"
+      class="advisor-input"
+    />
+  </label>
+
+  <label class="advisor-label">
+    <span>Year</span>
+    <input 
+      type="number" 
+      id="advisor-year" 
+      placeholder="4"
+      class="advisor-input"
+    />
+  </label>
+
+  <label class="advisor-label">
+    <span>Program</span>
+    <select id="advisor-program" class="advisor-input">
+      <option>Computer Science</option>
+      <option>Software Engineering</option>
+      <option>Data Science</option>
+      <option>Other</option>
+    </select>
+  </label>
+</section>
